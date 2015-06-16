d07d9b2b0c80aafbaf1752c90d774c9e9eaf6922
Daniel Diniz <shadowelement@gmail.com>
2015-06-16 17:56:01 -0300

    Clean code; +Observer and Singleton design patterns

--- src/Main.java
+++ src/Main.java
@@ -10,143 +10,159 @@ import controlP5.*;
 
 
 
-public class DecoratorTest extends PApplet {
-  //	An array of circles
-	
-  private static Shape[] circles = new Shape[12];
-  
-  private final int MAX_SHAPES = 10;
-  private ArrayList<Shape> attractors;
-  private ArrayList<Shape> reppelers;
-  
-  private Shape mainAttractor;
-  
-  P5ControlPanel controlP5;
-  
-//  Shape attractor;
+public class Main extends PApplet {
 
-  public void setup() {
-	attractors = new ArrayList<Shape>();
-	reppelers  = new ArrayList<Shape>();
-	
-    size(600,600);
-    //frameRate(1);
-    //background(0);
-    
-    controlP5 = new P5ControlPanel(this);
-    
-    // Initialize all "circles"
-    for (int i = 0; i < circles.length; i++) {
-     circles[i] = new Circle(this);
-    }
-    
-    mainAttractor = new Circle(this);
-    mainAttractor.setPos(new PVector(width/2,height/2));
-    mainAttractor.setSpeed(new PVector(0,0));
-  }
-  
-  public void reset() {
-	  attractors.clear();
-	  reppelers.clear();
-	  
-	  for (int i = 0; i < circles.length; i++) {
-		  circles[i] = new Circle(this);
-	  }
-	  
-	  mainAttractor = new Circle(this);
-	  mainAttractor.setPos(new PVector(width/2,height/2));
-	  mainAttractor.setSpeed(new PVector(0,0));
-  }
+	// singleton instance of the main simulation object
+	private static Main mainInstanceSingleton = null;
 
-  public synchronized void draw() {
-	  
-    //background(100,10);
-	// Draw a rect over all elements with some alpha to create a "trail" illusion
-    fill(111,255-controlP5.getControllerValue("Trail"));
-    rect(0,0,width,height);
+	//	An array of circles
+	private static Shape[] circles = new Shape[12];
 
-    mainAttractor.run();
-    
-    // Move and display all circles
-    for (int i = 0; i < circles.length; i++) {
-      circles[i].run();
-      circles[i].setR(controlP5.getControllerValue("Size"));
-      circles[i].setAmount(controlP5.getControllerValue("RepelIntensity"));
-      
-      if(controlP5.getControllerValue("Vectors")==1)
-    	  circles[i].drawVectors();
-    	
-    }
-    
-    if(controlP5.getControllerValue("Connect")==1)
-    	connectShapes();
-    
-    
-    for (Shape attrac : attractors) {
-    	attrac.run();
-    }
-    
-    for (Shape reppel : reppelers) {
-    	reppel.run();
-    }
-  }
- 
-  
-  public static Shape[] getShapes(){
-	  return circles;
-  }
-  
-  private void connectShapes(){
-	  for (int i = 0; i < circles.length; i++) {
-		  //for (int j = circles.length/2-1; j < circles.length; j++) {
-		  for (int j = i; j < circles.length; j++) {
-			  strokeWeight(1);
-			  stroke(0);
-		      //stroke(random(255),random(255),random(255));
-			  line(circles[i].getPos().x, circles[i].getPos().y, circles[j].getPos().x, circles[j].getPos().y);
-		  }
-	    }
-  }
-  
-	public void controlEvent(ControlEvent theEvent) {
-		  
-		if(theEvent.isController()) { 
-			if(theEvent.controller().name()=="Attract") {
-				mainAttractor = new AttractShape(mainAttractor);
-//				for (int i = 0; i < circles.length; i++) {
-//				      circles[i] = new AttractShape(circles[i]);;
-//				}
-			}
-			if(theEvent.controller().name()=="Repel") {
-			    //Add "repel" behaviour to all circles...
-			    for (int i = 0; i < circles.length; i++) {
-			      circles[i] = new RepelShape(circles[i],1);;
-			  	}
+	private final int MAX_SHAPES = 10;
+	private ArrayList<Shape> attractors;
+	private ArrayList<Shape> reppelers;
 
-			}
-			if(theEvent.controller().name()=="Reset") {
-				reset();
+	//  main attractor shape (in the middle)
+	private Shape mainAttractor;
+
+	// instance of the control panel singleton
+	P5ControlPanel controlP5;
+
+	public static Main getInstance() {
+		/* does not need to check for existing
+		 * instances; automatically instantiated
+		 * at first run						  */	
+		return mainInstanceSingleton;
+	}
+	 // ===========================================================
+	 /*	 setup @Overrides a processing function. It's charged with 
+	  *	 setting the whole simulation sketch up 	   			 */
+	@Override
+	public void setup() {
+		/* sets up singleton instance to be recovered
+		 * by other classes 					   */
+		mainInstanceSingleton = this;
+
+		attractors = new ArrayList<Shape>();
+		reppelers  = new ArrayList<Shape>();
+
+		size(600,600);
+
+		// gets singleton
+		controlP5 = P5ControlPanel.getInstance(this);
+
+		// initialize all "circles"
+		for (int i = 0; i < circles.length; i++) {
+			circles[i] = new Circle(this);
+		}
+		
+		// sets the main attractor in the middle
+		mainAttractor = new Circle(this);
+		mainAttractor.setPos(new PVector(width/2,height/2));
+		mainAttractor.setSpeed(new PVector(0,0));
+	}
+
+	// ===========================================================
+	// reset the whole sketch to factory settings
+	public void reset() {
+		attractors.clear();
+		reppelers.clear();
+
+		for (int i = 0; i < circles.length; i++) {
+			circles[i] = new Circle(this);
+		}
+
+		mainAttractor = new Circle(this);
+		mainAttractor.setPos(new PVector(width/2,height/2));
+		mainAttractor.setSpeed(new PVector(0,0));
+	}
+	// ===========================================================
+	// responsible for redrawing the sketch simulation every cycle
+	public synchronized void draw() {
+
+		/* Draw a rect over all elements with some alpha to create
+		 * a "trail" illusion									*/
+		fill(111,255-controlP5.getControllerValue("Trail"));
+		rect(0,0,width,height);
+
+		mainAttractor.run();
+
+		// Display and move all circles
+		for (int i = 0; i < circles.length; i++) {
+			circles[i].run();
+			// sets size and repel intensity according to controller
+			circles[i].setR(controlP5.getControllerValue("Size"));
+			circles[i].setAmount(controlP5.getControllerValue("RepelIntensity"));
+
+			// check if Vector button is pressed
+			if(controlP5.getControllerValue("Vectors")==1)
+				circles[i].drawVectors();
+		}
+
+		// check if Connect button is pressed
+		if(controlP5.getControllerValue("Connect")==1)
+			connectShapes();
+
+		for (Shape attrac : attractors) {
+			attrac.run();
+		}
+
+		for (Shape reppel : reppelers) {
+			reppel.run();
+		}
+	}
+
+
+	public static Shape[] getShapes(){
+		return circles;
+	}
+
+	private void connectShapes(){
+		for (int i = 0; i < circles.length; i++) {
+			
+			for (int j = i; j < circles.length; j++) {
+				strokeWeight(1);
+				stroke(0);
+				//stroke(random(255),random(255),random(255));
+				line(circles[i].getPos().x, circles[i].getPos().y, circles[j].getPos().x, circles[j].getPos().y);
 			}
 		}
 	}
-	
+
+    // ===========================================================
+	// both attract and repel button behaviors
+	public void attractButton() {
+		//Add "attract" behavior to the middle circle
+		this.mainAttractor = new AttractShape(mainAttractor);
+	}
+
+	public void repelButton() {
+		//Add "repel" behavior to all circles
+		for (int i = 0; i < circles.length; i++) {
+			circles[i] = new RepelShape(circles[i],1);;
+		}
+	}
+	//	 =========================================================
+	/*	 listens for key press from the "spacebar" or "enter" keys
+	 * 	 and handles creation of attracting or repelling shapes.*/
 	public synchronized void keyPressed(KeyEvent e) {
 		Random r = new Random();
 
 		if (e.getKeyChar() == KeyEvent.VK_SPACE && attractors.size() < MAX_SHAPES) {
-	    	Shape attractor = new Circle(this);
-	    	attractor = new AttractShape(attractor);
-	    	attractor.setPos(new PVector(r.nextInt(500) + 50,r.nextInt(500) + 50));
-	    	attractor.setR(10);
-	    	attractors.add(attractor);
+			Shape attractor = new Circle(this);
+			attractor = new AttractShape(attractor);
+			attractor.setPos(new PVector(r.nextInt(500) + 50,r.nextInt(500) + 50));
+			attractor.setR(10);
+			attractors.add(attractor);
 		}
+		
 		else if (e.getKeyChar() == KeyEvent.VK_ENTER && reppelers.size() < MAX_SHAPES) {
 			Shape reppeler = new Circle(this);
-	    	reppeler = new RepelShape(reppeler, 2);
-	    	reppeler.setPos(new PVector(r.nextInt(550) + 50,r.nextInt(550) + 50));
-	    	reppeler.setR(70);
-	    	reppelers.add(reppeler);
+			reppeler = new RepelShape(reppeler, 2);
+			reppeler.setPos(new PVector(r.nextInt(550) + 50,r.nextInt(550) + 50));
+			reppeler.setR(70);
+			reppelers.add(reppeler);
 		}
 	}
-	
+
 }
--- src/P5ControlPanel.java
+++ src/P5ControlPanel.java
@@ -7,29 +7,6 @@ import processing.core.*;
 
 public class P5ControlPanel extends PApplet {
 
-	class Info {
-		  float a;
-		  float n = 0;
-		  String txt = "";
-		  Textarea label;
-
-		  Info() {
-		    label = controlP5.addTextarea("Hello\nWorld")
-		               .setSize(200,200)
-		               .setPosition(300,40)
-		               .setColor(color(255))
-		               .setColorBackground(color(100,0))
-		               .setLineHeight(12);
-
-		  }
-
-		  void update() {
-		    a += (n-a)*0.1;
-		    label.setColorBackground(color(100,255*a));
-		  }
-		}
-
-
 	// ControlP5 Singleton - Instanciation, Setup and Event Controller
 
 	private static PApplet controlParent = null;
@@ -38,28 +15,8 @@ public class P5ControlPanel extends PApplet {
 
 	protected P5ControlPanel(PApplet source) {
 
-		CallbackListener cb;
-		Info info;
-
-		cb = new CallbackListener() {
-
-			public void controlEvent(CallbackEvent theEvent) {
-				switch(theEvent.getAction()) {
-				case(ControlP5.ACTION_ENTER):
-					info.n = 1;
-				info.label.setText(theEvent.getController().getInfo());
-				cursor(HAND);
-				break;
-				case(ControlP5.ACTION_LEAVE):
-				case(ControlP5.ACTION_RELEASEDOUTSIDE):
-					info.n = 0;
-				cursor(ARROW);
-				break;
-				}
-			}
-		};
-
 		controlP5 = new ControlP5(source);
+		controlP5.addCallback(createCallBackListener());
 
 		// change the default font to Verdana
 		PFont p = createFont("Verdana",9);
@@ -93,63 +50,40 @@ public class P5ControlPanel extends PApplet {
 			return instance;
 	}
 
-	// handles each Control Event - forwarded here from the main class
-	public void controlEvent(ControlEvent theEvent) {
-
-		if(theEvent.isController()) {
-
-			print("control event from : "+theEvent.controller().name());
-			println(", value : "+theEvent.controller().value());
-
-			// clicking on bang1 sets toggle1 value to 1 (true)
-			if(theEvent.controller().name()=="Attract") {
-				System.out.println("yeap");
-				controlP5.controller("toggle1").setValue(1);
-			}
-
-			if(theEvent.controller().name()=="Attract") {
-				Main.getInstance().attractBehavior();
-			}
-
-			if(theEvent.controller().name()=="Repel") {
-				Main.getInstance().repelBehavior();
-
-			}
-			if(theEvent.controller().name()=="Reset") {
-				Main.getInstance().reset();
-			}
-		}
-
-	}
-
-	public synchronized void keyPressed(KeyEvent e) {
-		Random r = new Random();
-
-		if (e.getKeyChar() == KeyEvent.VK_SPACE ) {
-			//				&& attractors.size() < MAX_SHAPES) {
-
-			System.out.println("PEGOU?");
-
-			/*			Shape attractor = new Circle(this);
-			attractor = new AttractShape(attractor);
-			attractor.setPos(new PVector(r.nextInt(500) + 50,r.nextInt(500) + 50));
-			attractor.setR(10);
-			attractors.add(attractor);*/
-		}
-
-		else if (e.getKeyChar() == KeyEvent.VK_ENTER) {
-			System.out.println("PEGOU?");
-			//				&& reppelers.size() < MAX_SHAPES) {
-			/*			Shape reppeler = new Circle(this);
-			reppeler = new RepelShape(reppeler, 1);
-			reppeler.setPos(new PVector(r.nextInt(550) + 50,r.nextInt(550) + 50));
-			reppeler.setR(70);
-			reppelers.add(reppeler);*/
-		}
-	}
-
+	//returns values of controller elements.
 	public float getControllerValue(String c){
 		return controlP5.controller(c).value();
+	}
+
+
+	/*	 creates a callback listener so the panel knows about incoming
+	 *   control events												*/
+	private CallbackListener createCallBackListener() {
+		CallbackListener cb;	
+
+		cb = new CallbackListener() {
+
+			public void controlEvent(CallbackEvent theEvent) {
+
+				if(theEvent.getAction()==controlP5.ACTION_PRESSED) {
+
+					switch (theEvent.getController().getLabel()) {
+
+					case("Reset"):
+						Main.getInstance().reset();
+					break;
+					case("Attract"):
+						Main.getInstance().attractButton();
+					break;
+					case("Repel"):
+						Main.getInstance().repelButton();
+					break;
+					}
+				}
+			}
+		};
+
+		return cb;
 	}
 
 }
